6b34f9f041b559d16555cf72a802cd0142731860
Eigil Krogh Sorensen <eigilkrogh@icloud.com>
2024-04-21 15:42:37 +0200

    Add project files.

---
+++ SetGlobalVersion/ToolWindows/MyToolWindow.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.Imaging;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SetGlobalVersion
+{
+	public class MyToolWindow : BaseToolWindow<MyToolWindow>
+	{
+		public override string GetTitle(int toolWindowId) => "My Tool Window";
+
+		public override Type PaneType => typeof(Pane);
+
+		public override Task<FrameworkElement> CreateAsync(int toolWindowId, CancellationToken cancellationToken)
+		{
+			return Task.FromResult<FrameworkElement>(new MyToolWindowControl());
+		}
+
+		[Guid("a6806b75-2075-474c-875f-7fbc9c95bf31")]
+		internal class Pane : ToolkitToolWindowPane
+		{
+			public Pane()
+			{
+				BitmapImageMoniker = KnownMonikers.ToolWindow;
+			}
+		}
+	}
+}
---
+++ SetGlobalVersion/ToolWindows/MyToolWindowControl.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace SetGlobalVersion
+{
+	public partial class MyToolWindowControl : UserControl
+	{
+		public MyToolWindowControl()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, RoutedEventArgs e)
+		{
+			VS.MessageBox.Show("SetGlobalVersion", "Button clicked");
+		}
+	}
+}
